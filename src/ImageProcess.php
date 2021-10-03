285a6f19d24ffcd74fb8b6b07244e4fc4f28c73a
yannlo <73764168+yannlo@users.noreply.github.com>
2021-10-03 12:14:39 +0000

    process image v1

--- src/ImageProcess.php
+++ src/ImageProcess.php
@@ -4,31 +4,100 @@ namespace Yannlo\ImageRessource;
 
 use Psr\Http\Message\UploadedFileInterface;
 
+/**
+ * ImageProcess
+ * permit to process all images
+ */
 class ImageProcess
 {
     private array $images;
     private \PDO $pdo;
-    public function __construct(array $data,\PDO $pdo)
+    public function __construct(array $UploadedFiles, \PDO $pdo)
     {
         $this -> pdo = $pdo;
+
+        $count = count($UploadedFiles);
+        $allName = $this -> generateName($count);
+        foreach ($UploadedFiles as $file) {
+            $image = [];
+            $image["extension"] = $this -> generateExt($file);
+            $image["type"] = $this -> getType($file);
+            if ($count > 1) {
+                foreach ($allName as $id => $names) {
+                    $partImages = [];
+                    foreach ($names as $name) {
+                        $image["name"] = $name;
+                        $partImages[] = new Image($image);
+                    }
+                    $this -> images[$id] = $partImages;
+                }
+            } else {
+                foreach ($allName as $name) {
+                    $image["name"] = $name;
+                    $partImages[] = new Image($image);
+                }
+                $this -> images = $partImages;
+            }
+        }
     }
 
+    /**
+     * generateExt
+     *
+     * permite to generate all image name
+     *
+     * @param  UploadedFileInterface $file
+     * @return string
+     */
     public function generateExt(UploadedFileInterface $file): string
     {
         $name = $file-> getClientFileName();
         return substr($name, strpos($name, '.') + 1);
     }
 
-    public function generateName(string $name): array
+    /**
+     * generateName
+     *
+     * @return array
+     */
+    public function generateName(int $count = 1): array
     {
-        $request = $this ->pdo -> query("SELECT id FROM images LIMIT 1");
-        $id = $request->fetch()["id"];
-        $sizes= [Image::LITTLE_SIZE,Image::MEDIUM_SIZE,Image::BIG_SIZE];
-        $names =[];
-        foreach ($sizes as $size)
-        {
-            $names[] = "img".($id+1)."_".$size;
+
+        $request = $this ->pdo -> query("SELECT id FROM images ORDER BY id DESC LIMIT 1");
+        $id = (int) $request->fetch()["id"];
+        $names = [];
+        $sizes = [Image::LITTLE_SIZE,Image::MEDIUM_SIZE,Image::BIG_SIZE];
+        if ($count === 1) {
+            foreach ($sizes as $size) {
+                $names[] = "img" . ($id + 1) . "_" . $size;
+            }
+            return $names;
         }
+
+        for ($i = 1; $i <= $count; $i++) {
+            $namesPart = [];
+            foreach ($sizes as $size) {
+                $namesPart[] = "img" . ($id + $i) . "_" . $size;
+            }
+            $names[($id + $i)] = $namesPart;
+        }
+
         return $names;
+    }
+
+    /**
+     * getType
+     *
+     * @param  UploadedFileInterface $file
+     * @return string
+     */
+    public function getType(UploadedFileInterface $file): string
+    {
+         return $file -> getClientMediaType();
+    }
+
+    public function getImages(): array
+    {
+        return $this -> images;
     }
 }
--- tests/ImageProcessTest.php
+++ tests/ImageProcessTest.php
@@ -2,21 +2,22 @@
 
 namespace Yannlo\Tests;
 
-use GuzzleHttp\Psr7\ServerRequest;
-use GuzzleHttp\Psr7\UploadedFile;
 use PHPUnit\Framework\TestCase;
+use Yannlo\ImageRessource\Image;
+use GuzzleHttp\Psr7\UploadedFile;
+use GuzzleHttp\Psr7\ServerRequest;
 use Yannlo\ImageRessource\ImageProcess;
 
 class ImageProcessTest extends TestCase
 {
     public function setUp(): void
     {
-        $this -> pdo  = new \PDO("sqlite::memory");
+        $this -> pdo  = new \PDO("sqlite::memory:");
 
         $this -> pdo -> query("CREATE TABLE IF NOT EXISTS images (id INT PRIMARY KEY, type VARCHAR(20) , extension VARCHAR(3) ) ");
         $type = 'image/png' ;
         $ext = "png";
-        for ($i=1; $i <= 10; $i++) {
+        for ($i=1; $i <=10; $i++) {
             $request = $this -> pdo -> prepare("INSERT INTO images (id, type, extension) VALUES(:id, :type, :ext)");
             $request ->execute([
                 "id" => $i,
@@ -34,7 +35,7 @@ class ImageProcessTest extends TestCase
             "image/png"
         );
         $image2 = new UploadedFile(
-            "/tmp/phpf3er5",
+            "/tmp/phprver5",
             2000000,
             0,
             "image_conu2.png",
@@ -42,27 +43,75 @@ class ImageProcessTest extends TestCase
         );
         $uploadFiles =[
             "img1"=>$image1,
-            // "img2"=>$image2
+            "img2"=>$image2
         ];
-        $this->request -> withUploadedFiles($uploadFiles);
+        $this->request = $this->request -> withUploadedFiles($uploadFiles);
 
     }
 
 
     public function testGenerateNewName()
     {
-        $request = $this->request;
         $imageProcess = new ImageProcess([],$this->pdo);
-        $names = $imageProcess->generateName($request->getUploadedFiles()[0]);
+        $names = $imageProcess->generateName(1);
         $this -> assertSame(["img11_little","img11_medium","img11_big"],$names);
+    }
+
+    public function testGenerateMoreNewName()
+    {
+        $imageProcess = new ImageProcess([],$this->pdo);
+        $names = $imageProcess->generateName(2);
+        $result[11] = ["img11_little","img11_medium","img11_big"];
+        $result[12] = ["img12_little","img12_medium","img12_big"];
+        $this -> assertSame($result,$names);
     }
 
     public function testGetExtension()
     {
         $request = $this->request;
         $imageProcess = new ImageProcess([],$this->pdo);
-        $ext = $imageProcess->generateExt($request->getUploadedFiles()[0]);
+        
+        $ext = $imageProcess->generateExt($request->getUploadedFiles()['img1']);
         $this -> assertEquals("png",$ext);
+    }
+
+    public function testGetType()
+    {
+        $request = $this->request;
+        $imageProcess = new ImageProcess([],$this->pdo);
+        $ext = $imageProcess->getType($request->getUploadedFiles()['img1']);
+        $this -> assertEquals("image/png",$ext);
+    }
+
+    public function testGenerateImage()
+    {
+        $request = $this->request;
+        $files = $request->getUploadedFiles();
+        unset($files['img2']);
+        $imageProcess = new ImageProcess($files,$this->pdo); 
+        $result=[
+            new Image(["name"=>"img11_little","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img11_medium","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img11_big","type"=>"image/png","extension"=>"png"])
+        ];
+        $this ->assertEquals($result,$imageProcess->getImages());
+    }
+
+    public function testGenerateManyImages()
+    {
+        $request = $this->request;
+        $imageProcess = new ImageProcess($request->getUploadedFiles(),$this->pdo); 
+        $result[11]=[
+            new Image(["name"=>"img11_little","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img11_medium","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img11_big","type"=>"image/png","extension"=>"png"])
+        ];
+        $result[12]=[
+            new Image(["name"=>"img12_little","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img12_medium","type"=>"image/png","extension"=>"png"]),
+            new Image(["name"=>"img12_big","type"=>"image/png","extension"=>"png"])
+        ];
+        $this ->assertEquals($result,$imageProcess->getImages());
     }
 
 }
